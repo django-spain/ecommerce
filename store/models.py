7afc9c2b09e5fde9919b6e835bc33aa0ab7fb057
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-02-27 22:16:00 +0100

    174 Terminado con el carro

--- store/models.py
+++ store/models.py
@@ -22,3 +22,31 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
     
+    
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variant_category='color', is_active=True)
+    def tallas(self):
+        return super(VariationManager, self).filter(variant_category='talla', is_active=True)
+    
+    
+variation_category_choice = (
+    ('color', 'color'),
+    ('talla', 'talla'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variant_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variant_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+    
+    objects = VariationManager()
+    
+    def __str__(self):
+        return self.variant_category + ':' + self.variant_value
+    
+    
+    
+    
