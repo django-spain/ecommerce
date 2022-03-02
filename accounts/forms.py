5394ca6c2ce556576ddd8912773e3278bc3f87f6
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-03-02 23:56:13 +0100

    177 Registro de Usuarios

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,35 @@ from django import forms
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Ingrese Password',
+         'class' : 'form-control'
+    }))
+    
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Confirmar Password',
+        'class' : 'form-control'
+    }))
+    
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+        
+        
+    def __init__(self, *arg, **kwargs):
+        super(RegistrationForm, self).__init__(*arg, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Ingrese Nombre'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Ingrese Apellidos'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Ingrese Teléfono'
+        self.fields['email'].widget.attrs['placeholder'] = 'Ingrese Email'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+            
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "El password no coincide!"
+            )
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from accounts.forms import RegistrationForm
+from .models import Account
 
 # Create your views here.
 
 def register(request):
     form = RegistrationForm()
+    
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            # demo@demo.cl
+            username = email.split('@')[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name,
+                                               email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+            
     context = {
         'form' : form
     }
